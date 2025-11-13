ab3ec1c74522c17cd2aa854c086a9d441f3ed4a1
Andrew-Atwater <andrew.atwater@maine.edu>
2025-11-13 11:21:35 -0500

    Framework

---
+++ hash.py
@@ -0,0 +1,12 @@
+class DataItem:
+    def __init__(self, line):
+        #self.move_name
+        #self.genre
+        #self.release_date
+        #self.director
+        #self.box_office_rating
+        #self.rating
+        #self.duration_minutes
+        #self.production_company
+        #self.quote
+        pass
